b98af915f9913ba244755fff04ad1acedff41230
Artem Ptashenko <aptashenko2019@gmail.com>
2022-12-15 00:21:09 +0200

    add logic

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -4,16 +4,17 @@
     <hr class="w-[100%] h-5px" />
     <div class="max-w-[95%] calcWidth  mx-auto bg-white rounded-10px p-20px">
         <div class="flex overflow-x-scroll list">
-            <div v-for="({title, poster_path}, idx) in firstList" class="overflow-y-hidden min-h-350px flex flex-col justify-between w-200px flex-shrink-0 item" :key="idx">
+            <div v-for="({title, poster_path}, idx) in upcomingFilms" class="overflow-y-hidden min-h-350px flex flex-col justify-between w-200px flex-shrink-0 item" :key="idx">
                 <img class="rounded-5px cursor-pointer" :src="`https://image.tmdb.org/t/p/w500/${poster_path}`" />
                 <h2 class="font-bold text-12px">{{title}}</h2>
             </div>
         </div>
     </div>
-    <div class="flex w-[50%] mx-auto p-20px border-white border-2px rounded-10px">
-        <div>
-            <FormKit type="form" @submit="randomMovie" submit-label="Обрати кіно">
+    <div class="flex w-[50%] mt-20px mx-auto p-20px border-white border-2px rounded-10px flex-col">
+        <div class="w-[100%]">
+            <FormKit type="form" @submit="selectRandomMovie" submit-label="Обрати кіно">
                 <FormKit
+                    v-if="genres"
                     type="select"
                     name="genre"
                     label="Оберіть жанр"
@@ -21,11 +22,17 @@
                     />
                 <FormKit
                     type="select"
-                    name="country"
-                    label="Оберіть країну"
-                    :options="countries"
+                    name="year"
+                    label="Оберіть рік"
+                    :options="selectYears"
                     />
             </FormKit>
+        </div>
+        <div>
+            <div v-if="randomMovie" class="">
+                <img class="rounded-5px cursor-pointer" :src="`https://image.tmdb.org/t/p/w500/${randomMovie.poster_path}`" />
+                <h2 class="font-bold text-12px">{{randomMovie.title}}</h2>
+            </div>
         </div>
     </div>
 </div>
@@ -33,58 +40,50 @@
 
 <script>
 import axios from 'axios';
-import { ref, onBeforeMount } from 'vue';
-
 export default {
-    setup() {
-        const API_KEY = '64b9d48df1f5249c2e8adb2dbe6fd5f9';
-        axios.defaults.baseURL = `https://api.themoviedb.org/3`;
-
-        let firstList = ref([]);
-        let genres = ref([]);
-        let countries = ref([]);
-
-        let choosenGenre = ref(null)
-
-        onBeforeMount(() => {
-            fetchGenres(API_KEY)
-            fetchPopularMovies(API_KEY)
-            fetchContries(API_KEY)
-            searchMovie(API_KEY)
-
-        })
-
-        const fetchPopularMovies = async (apiKey) => {
-            const resp = await axios.get(`/movie/popular?api_key=${apiKey}&language=ru`);
-            firstList.value = resp.data.results;
-        }
-
-        const fetchGenres = async (apiKey) => {
-            const resp = await axios.get(`/genre/movie/list?api_key=${apiKey}&language=ru`)
-            genres.value = resp.data.genres.map(item => item.name)
-        }
-
-        const fetchContries = async (apiKey) => {
-            const resp = await axios.get(`/configuration/countries?api_key=${apiKey}`)
-            countries.value = resp.data.map(item => item.english_name)
-        }
-
-        const searchMovie = async (apiKey) => {
-            const resp = await axios.get(`/search/movie?&api_key=${apiKey}&language=ru&page=1&region=ireland&year=2022`)
-            console.log(resp)
-        }
-
-        const randomMovie = (value) => {
-            console.log(value)
-        }
-
+    data() {
         return {
-            firstList, 
-            genres, 
-            choosenGenre, 
-            randomMovie, 
-            countries
+            upcomingFilms: null,
+            genres: null,
+            randomMovie: null,
         }
+    },
+    methods: {
+        async fectchUpcomingFilms() {
+            const resp = await axios.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=64b9d48df1f5249c2e8adb2dbe6fd5f9&language=ru&page=1`);
+            this.upcomingFilms = resp.data.results;
+        },
+        async fetchGenres() {
+            const resp = await axios.get('https://api.themoviedb.org/3/genre/movie/list?api_key=64b9d48df1f5249c2e8adb2dbe6fd5f9&language=ru');
+            const entries = resp.data.genres.map(item => Object.values(item))
+            this.genres = Object.fromEntries(entries);
+        },
+        async fetchFilteredFilms(year, genre, random) {
+            const resp = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=64b9d48df1f5249c2e8adb2dbe6fd5f9&primary_release_year=${year}&with_genres=${genre}&sort_by=popularity.desc`);
+            this.randomMovie = resp.data.results[random];
+            console.log(this.randomMovie);
+        },
+        selectRandomMovie(e) {
+            const randomNumber = Math.floor(Math.random() * (0 - 19) + 19);
+            this.fetchFilteredFilms(e.year, e.genre, randomNumber);
+        }
+    },
+    computed: {
+        selectYears() {
+            let array = [];
+            const maxYear = new Date().getFullYear();
+            const minYear = 1950;
+
+            for (let i = minYear; i <= maxYear; i++) {
+                array.push(i);
+            } 
+
+            return array.reverse();
+        }
+    },
+    mounted() {
+        this.fectchUpcomingFilms();
+        this.fetchGenres();
     }
 }
 </script>
